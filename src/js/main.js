1255e38ce5635ac4b1c394695078b8edf901ee3d
Sergey Khmelevskoy <xs290@me.com>
2018-03-05 02:41:24 +0300

    sku desktop fixes

--- src/js/main.js
+++ src/js/main.js
@@ -589,20 +589,20 @@ $(document).ready(function() {
   ////////////
 
   // textarea autoExpand
-  _document
-    .one('focus.autoExpand', '.ui__textarea', function() {
-      var savedValue = this.value;
-      this.value = '';
-      this.baseScrollHeight = this.scrollHeight;
-      this.value = savedValue;
-    })
-    .on('input.autoExpand', '.ui__textarea', function() {
-      var minRows = this.getAttribute('data-min-rows') | 0,
-        rows;
-      this.rows = minRows;
-      rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
-      this.rows = minRows + rows;
-    });
+  // _document
+  //   .one('focus.autoExpand', '.ui__textarea', function() {
+  //     var savedValue = this.value;
+  //     this.value = '';
+  //     this.baseScrollHeight = this.scrollHeight;
+  //     this.value = savedValue;
+  //   })
+  //   .on('input.autoExpand', '.ui__textarea', function() {
+  //     var minRows = this.getAttribute('data-min-rows') | 0,
+  //       rows;
+  //     this.rows = minRows;
+  //     rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 12);
+  //     this.rows = minRows + rows;
+  //   });
 
   // Masked input
   function initMasks() {
